dea6174738097875bc96e9a0e837cc67d5c086f0
MaPePeR <MaPePeR@users.noreply.github.com>
2015-08-13 20:34:46 +0200

    Add horrible side texture and use toolforge-top.

--- src/main/java/mapeper/ticonautotoolstation/AutoToolStationBlock.java
+++ src/main/java/mapeper/ticonautotoolstation/AutoToolStationBlock.java
@@ -1,10 +1,14 @@
 package mapeper.ticonautotoolstation;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
 
 //Thanks to http://www.minecraftforge.net/wiki/Containers_and_GUIs
@@ -31,5 +35,27 @@ public class AutoToolStationBlock extends Block implements ITileEntityProvider
 		}
 
 		return true;
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public IIcon getIcon(int side, int meta)
+	{
+		if (side == 1) {
+			//Top
+			return icon[1];
+		}
+		return icon[0];
+	}
+
+	IIcon[] icon;
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerBlockIcons(IIconRegister register)
+	{
+		icon = new IIcon[2];
+		icon[0] = register.registerIcon("autotoolstation:autotoolstation/sides");
+		icon[1] = register.registerIcon("tinker:toolforge_top");
+		//https://github.com/SlimeKnights/TinkersConstruct/blob/master/resources/assets/tinker/textures/blocks/toolforge_top.png
 	}
 }
