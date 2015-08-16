0a84b122df21f0b27d86fc7a250f1e91a049fef2
MaPePeR <MaPePeR@users.noreply.github.com>
2015-08-16 17:28:43 +0200

    Drop contents when block is broken.

--- src/main/java/mapeper/ticonautotoolstation/AutoToolStationBlock.java
+++ src/main/java/mapeper/ticonautotoolstation/AutoToolStationBlock.java
@@ -6,10 +6,16 @@ import net.minecraft.block.Block;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
+
+import java.util.Random;
 
 //Thanks to http://www.minecraftforge.net/wiki/Containers_and_GUIs
 public class AutoToolStationBlock extends Block implements ITileEntityProvider
@@ -36,6 +42,49 @@ public class AutoToolStationBlock extends Block implements ITileEntityProvider
 		}
 
 		return true;
+	}
+
+
+	@Override
+	public void breakBlock(World world, int x, int y, int z, Block par5, int par6) {
+		dropItems(world, x, y, z);
+		super.breakBlock(world, x, y, z, par5, par6);
+	}
+
+	private void dropItems(World world, int x, int y, int z){
+		Random rand = new Random();
+
+		TileEntity tileEntity = world.getTileEntity(x, y, z);
+		if (!(tileEntity instanceof IInventory)) {
+			return;
+		}
+		IInventory inventory = (IInventory) tileEntity;
+
+		for (int i = 0; i < inventory.getSizeInventory(); i++) {
+			ItemStack item = inventory.getStackInSlot(i);
+
+			if (item != null && item.stackSize > 0) {
+
+				float rx = rand.nextFloat() * 0.8F + 0.1F;
+				float ry = rand.nextFloat() * 0.8F + 0.1F;
+				float rz = rand.nextFloat() * 0.8F + 0.1F;
+
+				EntityItem entityItem = new EntityItem(world,
+						x + rx, y + ry, z + rz,
+						item.copy());
+
+				if (item.hasTagCompound()) {
+					entityItem.getEntityItem().setTagCompound((NBTTagCompound) item.getTagCompound().copy());
+				}
+
+				float factor = 0.05F;
+				entityItem.motionX = rand.nextGaussian() * factor;
+				entityItem.motionY = rand.nextGaussian() * factor + 0.2F;
+				entityItem.motionZ = rand.nextGaussian() * factor;
+				world.spawnEntityInWorld(entityItem);
+				item.stackSize = 0;
+			}
+		}
 	}
 
 	@Override
