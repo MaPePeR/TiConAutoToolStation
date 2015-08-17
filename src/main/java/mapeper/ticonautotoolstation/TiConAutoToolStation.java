2f04cc745c80ec228a66359a0adc20eabbb908f1
MaPePeR <MaPePeR@users.noreply.github.com>
2015-08-17 15:06:48 +0200

    Version 1.0.1

--- src/main/java/mapeper/ticonautotoolstation/TiConAutoToolStation.java
+++ src/main/java/mapeper/ticonautotoolstation/TiConAutoToolStation.java
@@ -17,7 +17,7 @@ public class TiConAutoToolStation
 {
 
     public static final String MODID = "TiConAutoToolStation";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "@VERSION@";
 
     @Mod.Instance(MODID)
     public static TiConAutoToolStation instance;
